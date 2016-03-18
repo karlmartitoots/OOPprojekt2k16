73f0a2e30daa75b8d789ad7ec102ef678af7263e
PusH <heiti123@hotmail.com>
2016-03-18 16:12:18 +0200

    playerdisplay funktsionaalsus ja hiire funktsionaalsuse lisamine

--- Mäng/src/CreateScreen.java
+++ Mäng/src/CreateScreen.java
@@ -2,43 +2,107 @@
  * Created by PusH on 18.03.2016.
  */
 import java.awt.Graphics;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.awt.event.MouseMotionListener;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
-public class CreateScreen extends JFrame{
+public class CreateScreen extends JFrame implements MouseListener{
+    int x=0;
+    int mouseX=0;
+    int mouseY=0;
+    boolean menudisplayout=false;
     public CreateScreen()  {
-        addKeyListener(new DrawUus());
+
         setContentPane(new DrawPane());
 
         setTitle("Card Game");
         setSize(900,900);
-        setResizable(false);
-        setVisible(true);
+        setResizable(true);
+
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-
+        addMouseMotionListener(drawing2);
+        addMouseListener(this);
+        setVisible(true);
     }
-
-    int x;
-    public class DrawUus extends KeyAdapter{
-        public void keyPressed(Graphics g,KeyEvent e){
-            x+=1;
-            g.fillOval(x, 25, 25, 25);
+    MouseMotionListener drawing2 = new MouseMotionListener() {
+        public void mouseDragged(MouseEvent e) {
 
         }
+
+        public void mouseMoved(MouseEvent e) {
+
+            mouseY= e.getY();
+            if(300>mouseY){
+                if(menudisplayout=false){
+                    menudisplayout=true;
+                }
+            }
+            if (mouseY > 740 ) {
+                menudisplayout = true;
+                System.out.println("TEST");
+            }
+            repaint();
+        }
+    };
+
+
+
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
     }
+    @Override
+    public void mousePressed(MouseEvent e)
+    {
+        mouseY= e.getY();
+
+        System.out.println("test");
+        this.repaint();
+
+
+    }
+
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+    @Override
+    public void mouseExited(MouseEvent e)
+    {
+
+    }
+
+
+
+
+
+
+
+
+
     class DrawPane extends JPanel {
-        BufferedImage img;
+        BufferedImage displayout,displayin;
         DrawPane()  {
 
+
             try {
-                img = ImageIO.read(new File("Mäng\\playerdisplay.png"));
+                displayout = ImageIO.read(new File("Mäng\\playerdisplay.png"));
+                displayin = ImageIO.read(new File("Mäng\\playerdisplaysmall.png"));
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -46,7 +110,12 @@ public class CreateScreen extends JFrame{
         public void paintComponent(Graphics g){
             //draw on g here e.g.
 
-            g.drawImage(img,0,280,900,600,null);
+            if(menudisplayout==true) {
+                g.drawImage(displayout, 0, 280, 900, 600, null);
+            }
+            if(menudisplayout==false){
+                g.drawImage(displayin, 0, 280, 900, 600, null);
+            }
 
         }
     }
